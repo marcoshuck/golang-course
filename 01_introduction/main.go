a926e7c983e4bef5cc0289d7e3a923cae690c4fa
Marcos Huck <marcos@huck.com.ar>
2020-10-11 20:03:35 -0300

    Add comments to hello world.

--- 01_introduction/main.go
+++ 01_introduction/main.go
@@ -1,7 +1,11 @@
+// Declaring package
 package main
 
+// Importing packages
 import "fmt"
 
+// Program main function.
 func main() {
+	// Using fmt package to print a message in the console.
 	fmt.Println("Hello, world!")
 }
